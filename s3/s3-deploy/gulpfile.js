59b0e78fc748cd7911ab896dfb61b7631a93d946
aratius <arata1129matsu@icloud.com>
2021-03-08 00:50:05 +0900

    gulpからs3へのデプロイ--コメント追加

--- s3/s3-deploy/gulpfile.js
+++ s3/s3-deploy/gulpfile.js
@@ -1,18 +1,19 @@
 const gulp = require("gulp")
 const awspublish = require("gulp-awspublish")
 
-gulp.task("deploy", (cb) => {
+// aws s3 へのデプロイ
+gulp.task("deploy", (done) => {
   const publisher = awspublish.create({
-    "accessKeyId": "AKIAUWPUYX7L23ENHQEG",
-    "secretAccessKey": "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",
+    "accessKeyId": "AKIAUWPUYX7L23ENHQEG",  //ここには記述したくない
+    "secretAccessKey": "TXG4XQc90kSZ7t9lD9ZzLF4eXsoJwyyHv2xXBw1z",  //ここには記述したくない
     "params": {
       "Bucket": "www.aualrxse.com"
     },
-    "endpoint": "s3-ap-northeast-1.amazonaws.com"
+    "endpoint": "s3-ap-northeast-1.amazonaws.com"  //公開先のエンドポイント
   })
   gulp.src("build/**/*")
     .pipe(publisher.publish())
     .pipe(publisher.sync())
     .pipe(awspublish.reporter())
-  cb()
+  done()
 })
