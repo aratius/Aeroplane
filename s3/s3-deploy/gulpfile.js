805c19df4bfbda0996cbcb12daa2f267a5084dc3
aratius <arata1129matsu@icloud.com>
2021-03-08 16:44:08 +0900

    AWSキー類はホームディレクトリを見にいく

--- s3/s3-deploy/gulpfile.js
+++ s3/s3-deploy/gulpfile.js
@@ -4,8 +4,9 @@ const awspublish = require("gulp-awspublish")
 // aws s3 へのデプロイ
 gulp.task("deploy", (done) => {
   const publisher = awspublish.create({
-    "accessKeyId": "AKIAUWPUYX7L23ENHQEG",  //ここには記述したくない
-    "secretAccessKey": "TXG4XQc90kSZ7t9lD9ZzLF4eXsoJwyyHv2xXBw1z",  //ここには記述したくない
+    // 多分 ~/ .aws/ credentials をみにいってくれるようになった
+    // "accessKeyId": "AKIAUWPUYX7L23ENHQEG",  //ここには記述したくない
+    // "secretAccessKey": "TXG4XQc90kSZ7t9lD9ZzLF4eXsoJwyyHv2xXBw1z",  //ここには記述したくない
     "params": {
       "Bucket": "www.aualrxse.com"
     },
